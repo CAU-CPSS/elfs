616c096002bf25315476f8d4e53943aaa8e4b293
genius-chang <hmutimg@gmail.com>
2025-08-14 02:47:15 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,104 @@
+import importlib
+import os
+import sys
+import inspect
+from pathlib import Path
+
+import pynvml
+import hydra
+from omegaconf import DictConfig
+
+from src.server.fedavg import FedAvgServer
+
+FLBENCH_ROOT = Path(__file__).parent.absolute()
+if FLBENCH_ROOT not in sys.path:
+    sys.path.append(FLBENCH_ROOT.as_posix())
+
+from src.utils.tools import parse_args
+
+
+@hydra.main(config_path="config", config_name="defaults", version_base=None)
+def main(config: DictConfig):
+    method_name = config.method.lower()
+
+    try:
+        fl_method_server_module = importlib.import_module(f"src.server.{method_name}")
+    except:
+        raise ImportError(f"Can't import `src.server.{method_name}`.")
+
+    module_attributes = inspect.getmembers(fl_method_server_module)
+    server_class = [
+        attribute
+        for attribute in module_attributes
+        if attribute[0].lower() == method_name + "server"
+    ][0][1]
+
+    get_method_hyperparams_func = getattr(server_class, f"get_hyperparams", None)
+
+    config = parse_args(config, method_name, get_method_hyperparams_func)
+
+    # target method is not inherited from FedAvgServer
+    if server_class.__bases__[0] != FedAvgServer and server_class != FedAvgServer:
+        parent_server_class = server_class.__bases__[0]
+        if hasattr(parent_server_class, "get_hyperparams"):
+            get_parent_method_hyperparams_func = getattr(
+                parent_server_class, f"get_hyperparams", None
+            )
+            # class name: <METHOD_NAME>Server, only want <METHOD_NAME>
+            parent_method_name = parent_server_class.__name__.lower()[:-6]
+            # extract the hyperparameters of the parent method
+            parent_config = parse_args(
+                config, parent_method_name, get_parent_method_hyperparams_func
+            )
+            setattr(
+                config, parent_method_name, getattr(parent_config, parent_method_name)
+            )
+
+    if config.mode == "parallel":
+        import ray
+
+        num_available_gpus = config.parallel.num_gpus
+        num_available_cpus = config.parallel.num_cpus
+        if num_available_gpus is None:
+            pynvml.nvmlInit()
+            num_total_gpus = pynvml.nvmlDeviceGetCount()
+            if "CUDA_VISIBLE_DEVICES" in os.environ.keys():
+                num_available_gpus = min(
+                    len(os.environ["CUDA_VISIBLE_DEVICES"].split(",")), num_total_gpus
+                )
+            else:
+                num_available_gpus = num_total_gpus
+        if num_available_cpus is None:
+            num_available_cpus = os.cpu_count()
+        try:
+            ray.init(
+                address=config.parallel.ray_cluster_addr,
+                namespace=method_name,
+                num_cpus=num_available_cpus,
+                num_gpus=num_available_gpus,
+                ignore_reinit_error=True,
+            )
+        except ValueError:
+            # have existing cluster
+            # then no pass num_cpus and num_gpus
+            ray.init(
+                address=config.parallel.ray_cluster_addr,
+                namespace=method_name,
+                ignore_reinit_error=True,
+            )
+
+        cluster_resources = ray.cluster_resources()
+        config.parallel.num_cpus = cluster_resources["CPU"]
+        config.parallel.num_gpus = cluster_resources["GPU"]
+
+    server = server_class(args=config)
+    server.run()
+
+
+if __name__ == "__main__":
+    # For gather the Fl-bench logs and hydra logs
+    # Otherwise the hydra logs are stored in ./outputs/...
+    sys.argv.append(
+        "hydra.run.dir=./out/${method}/${common.dataset}/${now:%Y-%m-%d-%H-%M-%S}"
+    )
+    main()
